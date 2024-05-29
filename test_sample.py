b51f7fffbe625c137b04dd2f454e6c3b8f1ca830
Evgeny Evseev <pelid80@gmail.com>
2024-05-29 13:09:00 +0300

    Skip broken autotest sample

--- test_sample.py
+++ test_sample.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def func(x):
     return x + 1
 
@@ -6,5 +9,6 @@ def test_correct_answer():
     assert func(4) == 5
 
 
+@pytest.mark.skip(reason="Example of failed autotest.")
 def test_wrong_answer():
     assert func(10) == 5
